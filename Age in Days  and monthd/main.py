b6d461b611852a2300cd34de8340a7df2f3cc6f2
jesse-aj <appiahjesse123@gmail.com>
2025-11-10 23:06:59 +0000

    Created a simple age calculator

---
+++ Age in Days  and monthd/main.py
@@ -0,0 +1,22 @@
+"""
+
+1. START
+2. DISPLAY A WECOME MESSAGE
+3. TELL USER TO ENTER NAME
+2. TELL THE USER TO INPUT AGE IN YEARS
+3.CALCULATE AGE IN MONTHS
+4.CALCULATE AGE IN DAYS
+5. DISPLAY AGE AND IN MONTHS AND IN DAYS
+6. END
+"""
+print (" == WELCOME TO AGE CALCULATOR == ")
+name = input("Enter your name ")
+age = int(input("Enter age in years "))
+ 
+
+age_months = age * 12
+age_days = age * 365
+
+print(f"\n AGE CALCULATOR FOR {name}")
+print(f"{age} in months is {age_months}")
+print(f"{age} in days is {age_days}")
