3ad599ba562da719950ebd68af285769d16f4a0b
jesse-aj <appiahjesse123@gmail.com>
2025-12-04 01:30:16 +0000

    did more practice on variable scopes

--- Modules/Variable/Enclosing.py
+++ Modules/Variable/Enclosing.py
@@ -1,16 +1,18 @@
-x = "global x"
+
+x = "global"
 
 
 def outer():
     x = "outer_x"  # local to outer
 
     def inner():
-        x = "inner_x" # local to inner
-        print(x) # prints local x according to LEGB  FOR INNER FUNCTION
+        # nonlocal x # to change the variable of our outer function
+        x = "inner_x"  # local to inner
+        print(x)  # prints local x according to LEGB  FOR INNER FUNCTION
     inner()
-    print(x) #prints local function for outer function(check identation)
+    print(x)  # prints local function for outer function(check identation)
+
 
 outer()
-
 
 print(x)
