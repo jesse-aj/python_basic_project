bc63f65404e12b8bb88dffb99ea8d99664b96a7d
jesse-aj <appiahjesse123@gmail.com>
2025-12-04 13:57:38 +0000

    worked more on data structures

---
+++ Data_structures/lists2.py
@@ -0,0 +1,10 @@
+# A list --> ordered and mutable data structure
+
+
+score = [40, 50, 60, 70, "Data", 450.43, False]
+
+print(type(score))  # prints the type od score
+print(score[3])  # prints values according to index
+
+score.append(102)
+print(score)
---
+++ Data_structures/tuple1.py
@@ -0,0 +1,9 @@
+#Tuples --> Ordered and immutable
+
+scores = (50, 537, 938)
+
+
+
+
+
+print(scores)
